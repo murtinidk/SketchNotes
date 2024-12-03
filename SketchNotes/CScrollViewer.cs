1916f5be5494be01b46158d5225068c3f0c19374
Martin Thalmayr <thalmayr@student.tugraz.at>
2024-12-03 22:44:28 +0100

    Add project files.

---
+++ SketchNotes/CScrollViewer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SketchNotes
+{
+    public class CScrollViewer : ScrollViewer
+    {
+        const int a4Height = 2970;
+        const int a4Width = 2100;
+
+        StackPanel pages = new StackPanel();
+
+        static float zoom = 10;
+        const float zoomStep = 0.02F;
+
+        public CScrollViewer()
+        {
+            this.Background = new SolidColorBrush(Colors.White);
+            this.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            this.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            pages.HorizontalAlignment = HorizontalAlignment.Center;
+            this.AddChild(pages);
+
+            pages.Children.Add(new Page());
+
+            SetZoom(zoom);
+        }
+
+        public bool LoadPages(string path)
+        {
+            try
+            {
+                pages.Children.Clear();
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    while(fs.Position < fs.Length)
+                    {
+                        Page page = new Page();
+                        page.LoadPage(fs);
+                        pages.Children.Add(page);
+                    }
+                }
+                SetZoom(zoom);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public bool SavePages(string path)
+        {
+            try
+            {
+
+                using (System.IO.FileStream fs = new System.IO.FileStream(path + ".temp", System.IO.FileMode.Create))
+                {
+                    fs.SetLength(0);
+                    foreach (var item in pages.Children)
+                    {
+                        if (item is Page && item != null)
+                        {
+                            Page page = item as Page;
+                            page.SavePage(fs);
+                        }
+                    }
+                }
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                System.IO.File.Move(path + ".temp", path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        protected override void OnMouseWheel(System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                zoom += (int)(e.Delta * zoomStep);
+                zoom = zoom < 10 ? 10 : zoom;
+                zoom = zoom > 500 ? 500 : zoom;
+                SetZoom(zoom);
+                base.UpdateLayout();
+            }
+            else
+            {
+                base.OnMouseWheel(e);
+            }
+        }
+
+
+        private void SetZoom(float zoom)
+        {
+            double verOffset = 0;
+            if ((int)base.ScrollableHeight != 0)
+            {
+                verOffset = base.VerticalOffset / base.ScrollableHeight;
+            }
+            double horOffset = 0;
+            if ((int)base.ScrollableWidth != 0)
+            { 
+                horOffset = (base.HorizontalOffset) / base.ScrollableWidth;
+            }
+
+            foreach (var item in pages.Children)
+            {
+                if (item is InkCanvas && item != null)
+                {
+                    InkCanvas inkCanvas = item as InkCanvas;
+                    inkCanvas.RenderTransform = new ScaleTransform(zoom / 100.0, zoom / 100.0);
+                    inkCanvas.Margin = new Thickness(0, 0, inkCanvas.Width * zoom / 100.0 - a4Width, 10 + inkCanvas.Height * zoom / 100.0 - a4Height);
+
+                    pages.Width = inkCanvas.RenderTransform.Transform(new Point(inkCanvas.Height, inkCanvas.Width)).Y;
+                    inkCanvas.HorizontalAlignment = HorizontalAlignment.Center;
+                }
+            }
+            base.UpdateLayout();
+            base.ScrollToVerticalOffset(verOffset * ScrollableHeight);
+            base.UpdateLayout();
+            base.ScrollToHorizontalOffset((horOffset) * ScrollableWidth);
+        }
+
+
+    }
+
+
+}
---
+++ SketchNotes/Page.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Ink;
+using System.IO;
+
+namespace SketchNotes
+{
+    internal class Page : InkCanvas
+    {
+        public Page()
+        {
+            Background = new SolidColorBrush(Colors.Gray);
+            Height = 2970;
+            Width = 2100;
+            ClipToBounds = true;
+            Focusable = false;
+            ResizeEnabled = true;
+            Margin = new Thickness(0, 0, 0, 10);
+        }
+
+        public void LoadPage(FileStream fileStream)
+        {
+            var transform = this.RenderTransform;
+            this.RenderTransform = null;
+            this.Strokes = new StrokeCollection(fileStream);
+            this.RenderTransform = transform;
+        }
+
+        public void SavePage(FileStream fileStream)
+        {
+            var transform = this.RenderTransform;
+            this.RenderTransform = null;
+            Strokes.Save(fileStream);
+            this.RenderTransform = transform;
+        }
+
+        public bool SavePage(string path)
+        {
+            try
+            {
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Create))
+                {
+                    Strokes.Save(fs);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
